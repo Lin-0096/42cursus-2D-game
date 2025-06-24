db62fe7490478c60cb972f1e65f71d3caa25d919
Lin Liu <liulin0096@gmail.com>
2025-06-24 19:35:01 +0000

    mlx

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,14 +6,19 @@
 /*   By: linliu <linliu@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 10:49:45 by linliu            #+#    #+#             */
-/*   Updated: 2025/06/24 14:20:17 by linliu           ###   ########.fr       */
+/*   Updated: 2025/06/24 19:26:28 by linliu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# define WIDTH 800
+# define HEIGHT 600
+
 # include "../MLX42/include/MLX42/MLX42.h"
 # include "../libft/libft.h"
+# include <stdlib.h> //exit
+# include <stdio.h>  //perror
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: linliu <linliu@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 10:46:49 by linliu            #+#    #+#             */
-/*   Updated: 2025/06/24 17:18:53 by linliu           ###   ########.fr       */
+/*   Updated: 2025/06/24 19:34:22 by linliu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,30 @@
 
 int	main(int argc, char **argv)
 {
-	if(argc < 2)
-	{
-		ft_putstr_fd("No map!\n", STDERR_FILENO);
-		exit(EXIT_FAILURE);
-	}
-	if(argc > 2)
-	{
-		ft_putstr_fd("One map at a time!\n", STDERR_FILENO);
-		exit(EXIT_FAILURE);
-	}
-	(void)argv;
+	// if(argc < 2)
+	// {
+	// 	ft_putstr_fd("No map!\n", STDERR_FILENO);
+	// 	exit(EXIT_FAILURE);
+	// }
+	// if(argc > 2)
+	// {
+	// 	ft_putstr_fd("One map at a time!\n", STDERR_FILENO);
+	// 	exit(EXIT_FAILURE);
+	// }
 
+	//creat a window
+	mlx_t *mlx = mlx_init(WIDTH, HEIGHT, "so_long", false); // false, not fullscreen
+	if (!mlx)
+	{
+		perror("mlx failed");
+		exit(EXIT_FAILURE);
+	}
+	//key
+	mlx_key_hook();
+	mlx_close_hook();
+
+	//run
+	mlx_loop(mlx);
+	//stop
+	mlx_terminate(mlx);
 }
